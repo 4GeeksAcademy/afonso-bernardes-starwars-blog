a37dedd41bc7df85c93f8f78715291c8e92f93ac
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-09-28 22:04:30 +0000

    Scroll bar to character cards. Loaded characters' info into card.

--- src/js/component/characterCard.js
+++ src/js/component/characterCard.js
@@ -1,14 +1,16 @@
 import React from "react";
-//import characterCard from "../../styles/characterCard.css"
+import characterCard from "../../styles/characterCard.css"
 
 export const CharacterCard = (props) => (
-    <div className="container text-left w-100 ps-0 me-3">
+    <div className="container text-left w-100 h-100 ps-0 me-3 mb-2">
         <div className="card bg-light">
             <img src="..." className="card-img-top" alt="..."></img>
             <div className="card-body">
-                <h5 className="card-title mb-3">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-outline-secondary">Learn more...</a>
+                <h4 className="card-title mb-3">{props.characterInfo.name}</h4>
+                <p className="card-text mb-0 pb-1">Gender: {props.characterInfo.gender}</p>
+                <p className="card-text mb-0 pb-1">Birth Year: {props.characterInfo.birth_year}</p>
+                <p className="card-text mb-0 pb-1">Height: {props.characterInfo.height}</p>
+                <a href="#" className="btn btn-outline-secondary mt-3">Learn more...</a>
             </div>
         </div>
     </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { CharacterCard } from "../component/characterCard";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="mx-4 px-4 pb-5">
-		<h2 className="text-white pb-4 pt-1">Characters</h2>
-		<div className="container-fluid d-flex justify-content-between ps-0">
-			<CharacterCard />
-			<CharacterCard />
-			<CharacterCard />
-			<CharacterCard />
-			<CharacterCard />
-			<CharacterCard />
-			<CharacterCard />
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="mx-4 px-4 pb-5">
+			<h2 className="text-white pb-4 pt-1">Characters</h2>
+			<div className="container-fluid overflow-auto d-flex justify-content-between ps-0">
+				{
+					store.charactersDetails.map( (element, idx) => {
+						return <CharacterCard key={idx} characterInfo={element} />;
+					})
+				}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
